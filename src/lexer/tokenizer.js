46b95a3e37479db56e51390db597eaf76561ccfe
NullDev (Shadow) <chris@nulldev.org>
2025-04-13 03:44:19 +0200

    Handle decimals and quotes

--- src/lexer/tokenizer.js
+++ src/lexer/tokenizer.js
@@ -20,12 +20,61 @@ class Tokenizer {
                 continue;
             }
 
+            if (char === '"'){
+                this.current++;
+                let str = "";
+                while (this.current < this.input.length && this.input[this.current] !== '"'){
+                    str += this.input[this.current++];
+                }
+                if (this.current >= this.input.length){
+                    throw new SyntaxError("Unterminated string literal");
+                }
+                this.current++;
+                this.tokens.push({ type: TokenType.String, value: str });
+                continue;
+            }
+
+            if (char === "'"){
+                this.current++;
+                if (this.current >= this.input.length){
+                    throw new SyntaxError("Unterminated character literal");
+                }
+                const charValue = this.input[this.current++];
+                if (this.current >= this.input.length || this.input[this.current] !== "'"){
+                    throw new SyntaxError("Unterminated character literal");
+                }
+                this.current++;
+                this.tokens.push({ type: TokenType.Char, value: charValue });
+                continue;
+            }
+
             if (/[0-9]/.test(char)){
                 let num = "";
-                while (/[0-9]/.test(this.input[this.current])){
+                let hasDecimal = false;
+                while (/[0-9]/.test(this.input[this.current]) || (this.input[this.current] === "." && !hasDecimal)){
+                    if (this.input[this.current] === "."){
+                        hasDecimal = true;
+                    }
                     num += this.input[this.current++];
                 }
                 this.tokens.push({ type: TokenType.Number, value: Number(num) });
+                continue;
+            }
+
+            if (char === "-"){
+                // Check if this is a unary minus
+                const prevToken = this.tokens[this.tokens.length - 1];
+                if (!prevToken ||
+                    prevToken.type === TokenType.LeftParen ||
+                    prevToken.type === TokenType.Plus ||
+                    prevToken.type === TokenType.Minus ||
+                    prevToken.type === TokenType.Multiply){
+                    this.tokens.push({ type: TokenType.UnaryMinus });
+                }
+                else {
+                    this.tokens.push({ type: TokenType.Minus });
+                }
+                this.current++;
                 continue;
             }
 
